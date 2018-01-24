e6dbd984647a0a6dcbaf4a8f2d3e6ec0741a7a99
Jordan Seres <seres_jordan@bah.com>
2018-01-24 13:50:25 -0500

    changed formatting

--- CMC.py
+++ CMC.py
@@ -3,22 +3,14 @@ import json
 import urllib2
 content = urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/?limit=7').read()
 coins = json.loads(content)
-
 counter=1;
-
-print '${:,.2f}'.format(12.121212)
-
 top7 = "---------------------\n"
+
 for c in coins:
 	price = json.dumps(c["price_usd"]).strip('"')
 	top7 += str(counter) + "." + (json.dumps(c["symbol"]).strip('"')) + "     "  + '${:,.2f}'.format(eval(price)) + "\n"
 	counter= counter + 1
 
-
 top7 += "---------------------"
-print(top7)
-
-data = {
-    "content": top7
-}
+data = {"content": top7}
 requests.post('https://discordapp.com/api/webhooks/404825031374405634/exHfIEsoOv6f6H13BSt0nNYgVJaCWkvmmcy6g4zAbd48WFrbazpdglRz2-xb1JV_E5KL', data=data)
